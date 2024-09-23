e98fa0370f2dc8a8d80bf0e6e2f410b461496bb4
Mamidivamsi <mamidivamsi46@gmail.com>
2024-09-23 17:21:26 +0530

    add submit page

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hello")
+
+const popup =document.getElementById('popup')
+
+
+function openpopup (){
+    popup.classList.add('open-popup')
+    console.log("hello")
+}
+function closepopup (){
+    popup.classList.remove('open-popup')
+    console.log("hello")
+}
+
+
